9666c6d8222e4578d45610ed436048410bca35b2
Ravi Teja Karanam <ravitejakaranam1999@gmail.com>
2024-10-30 20:44:53 -0400

    Button Hover Effects

---
+++ src/LineSlideOnHover.js
@@ -0,0 +1,7 @@
+import "./LineSlideOnHover.css";
+
+function LineSlideOnHover() {
+  return <button className="line-slide-on-hover">Hover Me</button>;
+}
+
+export default LineSlideOnHover;
---
+++ src/OuterBorderOnHover.js
@@ -0,0 +1,11 @@
+import "./OuterBorderOnHover.css";
+
+function OuterBorderOnHover() {
+  return (
+    <button className="outer-border-on-hover">
+      <span> Hover Me </span>
+    </button>
+  );
+}
+
+export default OuterBorderOnHover;
